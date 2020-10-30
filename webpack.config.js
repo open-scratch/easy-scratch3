e08811a71ed8c668411aef393a9edc0ced59094c
ChengYu <chengyu2333@gmail.com>
2020-10-30 17:07:24 +0800

    更新scratch素材库

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ const base = {
     devtool: 'cheap-module-source-map',
     devServer: {
         contentBase: path.resolve(__dirname, 'build'),
-        host: '0.0.0.0',
+        host: '127.0.0.1',
         port: process.env.PORT || 8601
     },
     output: {
